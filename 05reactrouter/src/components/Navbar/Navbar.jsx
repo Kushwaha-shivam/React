a1d8d7b064c694ec47c21fc14afce67e1e7e0482
Shivam <kushwahashivam050@gmail.com>
2024-04-15 06:49:28 +0530

    react-router-done

--- 05reactrouter/src/components/Navbar/Navbar.jsx
+++ 05reactrouter/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
             <header className="text-white body-font bg-black">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                        <Link to="/home" className="mr-5 font-semibold">Home</Link>
                         <Link to="/shop" className="mr-5 font-semibold">Shop</Link>
                         <Link to="/admin" className="mr-5 font-semibold">Admin</Link>
                         <Link to="/contact" className="mr-5 font-semibold">Contact</Link>
--- 05reactrouter/src/main.jsx
+++ 05reactrouter/src/main.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Admin from './components/Admin/Admin.jsx'
+import Shop from './components/Shop/Shop.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <App />
+
+        },
+        {
+            path: "/home",
+            element: <Home />
+        },
+        {
+            path: "/admin",
+            element: <Admin />
+        },
+        {
+            path: "/shop",
+            element: <Shop />
+        },
+        {
+            path: "/contact",
+            element: <Contact />
+        }
+    ]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )
